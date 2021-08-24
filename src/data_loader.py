ece566fbf13bd39b62a5938819751e226395eef3
prabhat-123 <srv.ale52@gmail.com>
2021-08-24 12:44:40 +0545

    features/pa/generating_sentence_embeddings/creating a python script to download the pretrained transformer model and to generate embeddings on the quora datasets

---
+++ src/data_loader.py
@@ -0,0 +1,26 @@
+import os
+
+import pandas as pd
+
+root_dir = os.getcwd()
+data_path = os.path.join(root_dir, 'dataset')
+
+class DataLoader():
+    
+    def __init__(self,file_name):
+        
+        self.file_name = file_name
+        if not os.path.exists(os.path.join(data_path, 'quora_data.csv')):
+            raise ValueError("Dataset is not available in the desired location.")
+
+    
+    def load_data(self, row_numbers):
+
+        self.row_numbers = row_numbers
+        file_path = os.path.join(data_path, self.file_name)
+        quora_data = pd.read_csv(file_path)
+        sentences = quora_data[:row_numbers]
+        return sentences
+
+    
+
